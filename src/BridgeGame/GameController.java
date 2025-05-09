b473bde4aa045a8d68df4a2e14184a2a46b6df2e
AwesomeAnsh69 <23parasharansh@gmail.com>
2025-05-08 17:03:50 -0700

    GameController class with instance variables and a constructor. Methods are not complete yet

--- src/BridgeGame/GameController.java
+++ src/BridgeGame/GameController.java
@@ -1,3 +1,44 @@
+import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.*;
+import javafx.event.*;
+import java.util.Random;
+
 public class GameController {
 
+    //instance variables
+    private Stage primaryStage;
+    private GridPane bridgePane;
+    private Button[][] tiles;
+    private boolean[][] isSafe;
+    private Random random;
+    private boolean gameOver;
+
+    //constructor
+    public GameController(){
+        gameOver = false;
+        isSafe = new boolean[2][6];
+        tiles = new Button[2][6];
+        random = new Random();
+        bridgePane = new GridPane();   
+    }
+
+    //methods
+    public void startGame(Stage primaryStage){
+        this.primaryStage = primaryStage;
+
+        //configuring the bridgePane
+        bridgePane.setAlignment(Pos.CENTER);
+        bridgePane.setHgap(10);
+        bridgePane.setVgap(5);
+        bridgePane.getChildren().clear();
+
+        //generating the bridge
+    }
+
+    public void generateBridge(){
+        
+    }
 }
